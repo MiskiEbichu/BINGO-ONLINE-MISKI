480ef4440bf31d300a93eec8fe9371f21560dcef
Miski Ebichu <elizacp9815@gmail.com>
2026-05-01 16:33:36 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,19 +1,9 @@
-function enviarFormulario() {
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
+function sortear(){
 
-    if (nombre && email && mensaje) {
-        alert(`Gracias, ${nombre}! Hemos recibido tu mensaje.`);
-        document.getElementById('nombre').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('mensaje').value = '';
-    } else {
-        alert('Por favor, completa todos los campos.');
-    }
-}
+let n = document.getElementById("numero").value;
 
-function comprarProducto(nombreProducto) {
-    const mensaje = encodeURIComponent(`Hola, estoy interesado en comprar el producto: ${nombreProducto}`);
-    window.open(`https://wa.me/51902023598?text=${mensaje}`, '_blank');
+localStorage.setItem("ultimoNumero",n);
+
+alert("Número cantado: "+n);
+
 }
