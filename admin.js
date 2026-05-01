1625d198633ac0d05a06c73fc893411ad840251e
Miski Ebichu <elizacp9815@gmail.com>
2026-05-01 17:20:53 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,38 @@
+let usados=[];
+
 function sortear(){
 
-let n=Math.floor(Math.random()*75)+1;
+let n;
+
+do{
+n=Math.floor(Math.random()*75)+1;
+}while(usados.includes(n));
+
+usados.push(n);
 
 document.getElementById("numero").innerText=n;
+localStorage.setItem("ultimo",n);
 
-localStorage.setItem("numeroActual",n);
+}
 
+setInterval(()=>{
+
+let u=localStorage.getItem("ultimo");
+
+if(document.getElementById("cantado")){
+document.getElementById("cantado").innerText=u;
+}
+
+},1000);
+
+function crearCartilla(){
+
+let nums=[];
+
+while(nums.length<25){
+let n=Math.floor(Math.random()*75)+1;
+if(!nums.includes(n)) nums.push(n);
+}
+
+return nums;
 }
