fa9a546800a36ada821df604e967aa2c417eb479
Miski Ebichu <elizacp9815@gmail.com>
2026-05-01 16:32:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-function enviarFormulario() {
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
+let numerosCantados = [];
 
-    if (nombre && email && mensaje) {
-        alert(`Gracias, ${nombre}! Hemos recibido tu mensaje.`);
-        document.getElementById('nombre').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('mensaje').value = '';
-    } else {
-        alert('Por favor, completa todos los campos.');
-    }
+function entrar(){
+
+let carton = [
+5,18,33,46,70,
+2,20,31,55,61,
+14,22,"FREE",52,66,
+8,27,40,57,73,
+12,19,35,48,64
+];
+
+mostrarCarton(carton);
+
 }
 
-function comprarProducto(nombreProducto) {
-    const mensaje = encodeURIComponent(`Hola, estoy interesado en comprar el producto: ${nombreProducto}`);
-    window.open(`https://wa.me/51902023598?text=${mensaje}`, '_blank');
+function mostrarCarton(datos){
+
+let html="";
+
+datos.forEach(n=>{
+
+html += `<div class="casilla" id="n${n}">${n}</div>`;
+
+});
+
+document.getElementById("carton").innerHTML = html;
+
+}
+
+function cantarNumero(num){
+
+numerosCantados.push(num);
+
+document.getElementById("cantados").innerHTML += num + " - ";
+
+let casilla = document.getElementById("n"+num);
+
+if(casilla){
+casilla.classList.add("marcado");
+}
+
 }
